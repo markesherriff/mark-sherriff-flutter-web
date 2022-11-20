5afe7f91dc4054376d2b18dc33ff2dd1a4f1f453
Mark Sherriff <markesherriff@gmail.com>
2022-11-20 14:16:38 -0500

    change title

--- main.dart.js
+++ main.dart.js
@@ -47237,14 +47237,14 @@ return A.a2f(this.r,s.b.Z(0,r.gt(r)),B.dR)}}
 A.ur.prototype={
 L(a){return this.e.$2(a,this.f)}}
 A.xS.prototype={
-L(a){return new A.pf(B.By,"Flutter Demo",A.a40(null,B.eu,null),null)}}
+L(a){return new A.pf(B.By,"sherriff",A.a40(null,B.eu,null),null)}}
 A.pq.prototype={
 au(){return new A.t_(B.l)}}
 A.t_.prototype={
 Jf(){this.ar(new A.WU(this))},
 L(a){var s,r=null
 this.a.toString
-s=A.a3Y("Flutter Demo Home Page",r)
+s=A.a3Y("Mark Sherriff",r)
 return new A.ql(new A.nF(s,new A.Eg(r,r,1/0,56),r),A.Il(A.a8O(A.a([B.GK,A.a3Y(""+this.d,A.bf(a).RG.e)],t.F),B.AZ),r,r),A.a2x(B.xG,!1,this.gJe(),"Increment"),r)}}
 A.WU.prototype={
 $0(){++this.a.d},
